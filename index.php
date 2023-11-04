013ca5d348f2a86f3d3362f217962efdbc1af225
cristianbonaguro <cricri070605@gmail.com>
2023-11-04 16:11:54 +0100

    Hero2

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <!-- header -->
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Il gusto del volo</title>
 </head>
 <body>
-    
+    <div id="hero" class="hero">
+    <div class="header">
+        <div class="nav">
+            <ul>
+                <li>Ciao</li>
+                <li class="second">Ciao</li>
+            </ul>
+        </div>
+        <div class="logo">
+            <img src="images/logo.png" alt="logo">
+        </div>
+        <div class="nav">
+            <ul>
+                <li>Ciao</li>
+                <li class="second">Ciao</li>
+            </ul>
+        </div>
+    </div>
+</div>
 </body>
+<script src="script.js"></script>
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+// let hero = 0;
+//     setTimeout(() => {
+//     if(hero%2==0)
+//         document.getElementById("hero").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(images/hero.jpg)" ;   
+//     else
+//     document.getElementById("hero").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(images/hero2.jpg)" ;
+//     hero++;
+// }, 5000);
